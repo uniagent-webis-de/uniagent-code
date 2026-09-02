2893213a880158b7bdb9283312bdbc27c32c9ad6
Maik Fröbe <maik.froebe@uni-jena.de>
2026-09-02 21:54:34 +0200

    mf

--- cikm26/text-processing/filter-spot-check-datasets.sh
+++ cikm26/text-processing/filter-spot-check-datasets.sh
@@ -2,4 +2,4 @@
 
 PATTERN='Dienstreise|Reisekosten|Trennungsgeld|Auslagenersatz|Fahrtkosten|Übernachtungsgeld|Tagegeld|Verpflegungsmehraufwand|Wegstreckenentschädigung|Reisekostenverordnung|Dienstreiseverordnung|A1-Bescheinigung'
 
-zcat legal-hessen-processed/documents.jsonl.gz | grep -iE "$PATTERN" |gzip > ../datasets/in-progress/retrieval-de-gesetze-spot-check/documents.jsonl.gz
+zcat legal-hessen-processed/documents.jsonl.gz | grep -iE "$PATTERN" |gzip > ../datasets/in-progress/retrieval-hessian-law-de-spot-check/documents.jsonl.gz
